4112cb53e3870939a69ac6f6c7e2dfdb8ee73e40
Rakesh <rakeshbr1508@gmail.com>
2024-09-19 16:49:51 +0530

    v7

--- v1/src/main/java/com/testng/selenium/v1/testng1.java
+++ v1/src/main/java/com/testng/selenium/v1/testng1.java
@@ -19,7 +19,7 @@ public class testng1
 
         // Initialize the class-level WebDriver (Chrome in this case)
         driver = (WebDriver) new ChromeDriver();
-        System.out.println("chrome Driver Initialized test10.");
+        System.out.println("chrome Driver Initialized test11.");
     }
 
     // Test case to load a URL and verify the page title
